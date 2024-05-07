25933ff8631bd2ec8e4a7271094b67ab601f0b52
h4lllllll <hjod1172@yahoo.co.jp>
2024-05-08 07:16:53 +0900

    fix

--- frontend/src/service/chats/functions.ts
+++ frontend/src/service/chats/functions.ts
@@ -63,25 +63,3 @@ export const getChatMembers = async (chatId: string): Promise<ChatMembers> => {
 
 	return res.data;
 };
-
-// "/chats/{chat-id}/members": {
-// 	/**
-// 	 * Get members from chats
-// 	 * @description A collection of all the members in the chat. Nullable.
-// 	 */
-// 	get: operations["chats.ListMembers"];
-
-// responses: {
-// 	"2XX": components["responses"]["microsoft.graph.conversationMemberCollectionResponse"];
-
-// "/chats/{chat-id}": {
-// 	/**
-// 	 * Get chat
-// 	 * @description Retrieve a single chat (without its messages). This method supports federation. To access a chat, at least one chat member must belong to the tenant the request initiated from.
-// 	 */
-// 	get: operations["chats.chat.GetChat"];
-// responses: {
-// 	/** @description Retrieved entity */
-// 	"2XX": {
-// 		content: {
-// 			"application/json": components["schemas"]["microsoft.graph.chat"];
